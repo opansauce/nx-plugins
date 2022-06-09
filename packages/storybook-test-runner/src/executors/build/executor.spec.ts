f0507e5ec4ae1a8187fba54c0246f43aa21eeed1
Joep Kockelkorn <joepkockelkorn@gmail.com>
2022-06-09 17:36:58 +0200

    feat: read package.json

--- packages/storybook-test-runner/src/executors/build/executor.spec.ts
+++ packages/storybook-test-runner/src/executors/build/executor.spec.ts
@@ -1,11 +1,42 @@
+const readJsonFile = jest.fn(() => ({
+  devDependencies: { test: '1.0.0' },
+  dependencies: { test: '1.0.0' },
+}));
 import { BuildExecutorSchema } from './schema';
 import executor from './executor';
+import type { ExecutorContext } from '@nrwl/devkit';
+
+jest.mock('@nrwl/devkit', () => ({
+  readJsonFile,
+}));
 
 const options: BuildExecutorSchema = {};
+const mockContext: ExecutorContext = {
+  root: '/root',
+  cwd: '/root',
+  isVerbose: true,
+  workspace: {
+    version: 2,
+    projects: {
+      proj: {
+        root: 'proj',
+        targets: {
+          'test-storybook': {
+            executor: '@opansauce/storybook-test-runner',
+          },
+        },
+      },
+    },
+    npmScope: 'test',
+  },
+  target: {
+    executor: '@opansauce/storybook-test-runner',
+  },
+};
 
 describe('Build Executor', () => {
   it('can run', async () => {
-    const output = await executor(options);
+    const output = await executor(options, mockContext);
     expect(output.success).toBe(true);
   });
 });
--- packages/storybook-test-runner/src/executors/build/executor.ts
+++ packages/storybook-test-runner/src/executors/build/executor.ts
@@ -1,9 +1,22 @@
+import type { ExecutorContext } from '@nrwl/devkit';
+import { readJsonFile } from '@nrwl/devkit';
+
 import { BuildExecutorSchema } from './schema';
 
-export default async function runExecutor(options: BuildExecutorSchema) {
+export interface PackageJson {
+  devDependencies?: Record<string, string>;
+}
+
+export default async function runExecutor(
+  options: BuildExecutorSchema,
+  context: ExecutorContext
+) {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const packageJson = readJsonFile<PackageJson>(`${context.root}/package.json`);
+
   /**
    * TODO:
-   * - [ ] check for dependencies:
+   * - [ ] check for dependencies with https://github.com/npm/node-semver:
    *   - "@storybook/test-runner": "^0.1.1"
    *   - "@storybook/jest": "^0.0.10"
    *   - concurrently?
@@ -15,7 +28,6 @@ export default async function runExecutor(options: BuildExecutorSchema) {
    * - [ ] kill when one exits
    * - [ ] support for watch mode?
    */
-  console.log('Executor ran for Build', options);
   return {
     success: true,
   };
