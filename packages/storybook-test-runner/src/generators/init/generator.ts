7d5a6dc5bbbf4b90e8a15d4f6d86f783dff5f87f
Joep Kockelkorn <joepkockelkorn@gmail.com>
2022-06-15 11:02:36 +0200

    feat: implement init generator

--- packages/storybook-test-runner/src/generators/init/generator.ts
+++ packages/storybook-test-runner/src/generators/init/generator.ts
@@ -1,25 +1,22 @@
 import {
   addDependenciesToPackageJson,
   readProjectConfiguration,
-  Tree,
+  updateProjectConfiguration,
 } from '@nrwl/devkit';
+import type { ProjectConfiguration, Tree } from '@nrwl/devkit';
 
 import { InitGeneratorSchema } from './schema';
 
 export default async function (tree: Tree, options: InitGeneratorSchema) {
   const projectConfig = readProjectConfiguration(tree, options.name);
 
-  if (typeof projectConfig.targets.storybook !== 'object') {
-    throw new Error('Storybook should be configured for this project');
-  }
-  // TODO: verify storybook 6.5.x is installed
+  assertStorybookTarget(projectConfig);
+  addNewTargets(tree, options.name, projectConfig);
 
   /**
    * TODO:
-   * - add addon interactions plugin to main.js
-   * - add test-storybook target
-   * - add e2e target
-   * - implement e2e
+   * - nice to have: verify storybook 6.5.x is installed
+   * - nice to have: add addon interactions plugin to main.js
    */
 
   return addDependenciesToPackageJson(
@@ -34,4 +31,34 @@ export default async function (tree: Tree, options: InitGeneratorSchema) {
       'wait-on': '^6.0.1',
     }
   );
+}
+
+function assertStorybookTarget(projectConfig: ProjectConfiguration) {
+  if (typeof projectConfig.targets.storybook !== 'object') {
+    throw new Error('Storybook should be configured for this project');
+  }
+}
+
+function addNewTargets(
+  tree: Tree,
+  name: string,
+  projectConfig: ProjectConfiguration
+) {
+  const storybookTarget = projectConfig.targets.storybook;
+
+  projectConfig.targets['test-storybook'] = {
+    executor: 'nx:run-commands',
+    options: {
+      command: `test-storybook --url http://localhost:${storybookTarget.options.port} -c ${storybookTarget.options.configDir}`,
+    },
+  };
+
+  projectConfig.targets['e2e'] = {
+    executor: 'nx:run-commands',
+    options: {
+      command: `concurrently -k -n storybook,test -s command-test "nx run ${name}:storybook" "wait-on tcp:${storybookTarget.options.port} && nx run ${name}:test-storybook"`,
+    },
+  };
+
+  updateProjectConfiguration(tree, name, projectConfig);
 }
